99622642f43b3e39481c02cdbdb8239ae651e307
yaosting <13761801969@163.com>
2019-05-28 14:44:45 +0800

    add two new model and new readme.md

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-### Real-time stream data fall detection via libsvm
+
+How to run:
+	cd /BR_SVM/python
+	python3 run_svm_predict.py seg3_train.txt
+
+
+note:
+1. This demo need 'pandas' to be installed, which is very hard to be installed on Raspberry.
+2. This BR_SVM/examples/feature_extraction.ipynb is used to Serialize batch training data
+3. This demo includes a trained model in BR_SVM/examples/seg3_train.txt/seg15_train.txt/seggroup_train.txt
+4. Please ingore the info 'Accuaracy = ..........' 
+
+
+more details in examples/...txt
+#training_testing_data_svm_acc_vel_timeseg3.txt is the all data
+#seg3_test.txt and seg3_train.txt  is the original data
+
+#seg3_test.txt.scale and seg3_train.txt.scale 
+ is the Scaled data
+ cmd: svm-scale seg3_train.txt > seg3_train.txt.scale
+
+#seg3_train.txt.range 
+ is the scale rule
+ cdm: svm-scale -s train.range seg3_train.txt > seg3_train.txt.scale
+     #使用train.range对test进行同样的缩放
+     svm-scale -r train.range seg3_test.txt > seg3_test.txt.scale
+
+#seg3_train.txt.scale.out and seg3_train.txt.scale.png
+ is the result of the grid
+
+#seg3_train.txt.model
+ cmd: svm-train.exe [options] training_set_file [model_file]
+ 1.rho #决策函数中的常数项的相反数（-b）
+ 2.svm的输出 y = y + model.sv_coef(i)*RBF(u,x);
+
+#seg3_test.txt.predict 
+ is the prediction for the  seg3_test.txt.scale   
+ cmd: svm-predict -b 1 test_file data_file.model output_file
+
+
+
+
+#we can use the model information to build the DecisionFunction (created by seg3_train.txt.model)
+
+%% DecisionFunction
+function plabel = DecisionFunction(x,model)
+
+gamma = model.Parameters(4);
+
+RBF = @(u,v)( exp(-gamma.*sum( (u-v).^2) ) );
+
+
+len = length(model.sv_coef);
+y = 0;
+
+for i = 1:len
+u = model.SVs(i,:);
+y = y + model.sv_coef(i)*RBF(u,x);
+end
+b = -model.rho;
+y = y + b;
+
+if y >= 0
+plabel = 1;
+else
+plabel = -1;
+end
---
+++ examples/seggroup_train.txt/seggroup.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import math
+from datetime import datetime
+def string_toDatetime(string):
+    string = str(string)
+    return datetime.strptime(string, '%H%M%S.%f')
+    #return datetime.strptime(string, '%Y%m%d%H%M%S.%f')
+
+
+FEATURE = ("mean", "max", "min", "std", "v_mean", "v_max", "v_min", "v_std")
+STATUS  = ("42", "57", "56")
+#status.index:0: 42--walking, 1:57--Fall, 2:56--Standing
+
+f = pd.read_excel('./training_testing_data.xlsx',frame_name='All').sort_values('label')
+print(f.head(5))
+Seg_granularity = 10
+gravity_data = []
+#with open(file_dir) as f:
+index = 0
+for i in range(0,len(f)):#len(f)
+    index = index + 1
+    label  = f['label'][i]
+    t = string_toDatetime(f['time'][i])
+    #act_label = f['act_label'][i]
+    #g = f['g'][i]
+    accX = float(f['accX'][i])
+    accY = float(f['accY'][i])
+    print (index)
+    velX = float(f['velX'][i])
+    vely = float(f['vely'][i])
+    
+    if accX == 0 or accY == 0 or velX == 0 or vely == 0:
+        continue
+    gravity = math.sqrt(math.pow(accX, 2)+math.pow(accY, 2))
+    velocity = math.sqrt(math.pow(velX, 2)+math.pow(vely, 2))
+    gravity_tuple = {"gravity": gravity, "velocity":velocity, "label": label, "time":t}
+    gravity_data.append(gravity_tuple)
+    
+splited_data = []
+cur_cluster  = []
+v_cluster = []
+counter      = 0
+last_status  = gravity_data[0]["label"]
+last_time = gravity_data[0]["time"]
+cur_time = gravity_data[0]["time"]
+print(len(gravity_data))
+for gravity_tuple in gravity_data:
+    #print('gravity_tuple',gravity_tuple)
+    print('cur_time',cur_time)
+    print('last_time',last_time)
+    twindow = cur_time-last_time
+    print('twindow',twindow)
+    if not (counter < Seg_granularity and gravity_tuple["label"] == last_status):
+        #print(counter,last_status)
+        seg_data = {"label": last_status, "values": cur_cluster, "v_values": v_cluster}
+        #print('seg_data', seg_data)
+        splited_data.append(seg_data)
+        #print('splited_data',splited_data)
+        cur_cluster = []
+        v_cluster = []
+        last_time = cur_time
+        #counter = 0
+    cur_cluster.append(gravity_tuple["gravity"])
+    v_cluster.append(gravity_tuple["velocity"])
+    last_status = gravity_tuple["label"]
+    cur_time = gravity_tuple['time']
+    #counter += 1
+# compute statistics of gravity data
+statistics_data = []
+for seg_data in splited_data:
+    np_values = np.array(seg_data.pop("values"))
+    #print('np_values',np_values)
+    seg_data["max"]  = np.amax(np_values)
+    seg_data["min"]  = np.amin(np_values)
+    seg_data["std"]  = np.std(np_values)
+    seg_data["mean"] = np.mean(np_values)
+    np_v_values = np.array(seg_data.pop("v_values"))
+    seg_data["v_max"]  = np.amax(np_v_values)
+    seg_data["v_min"]  = np.amin(np_v_values)
+    seg_data["v_std"]  = np.std(np_v_values)
+    seg_data["v_mean"] = np.mean(np_v_values)
+    statistics_data.append(seg_data)
+print('statsdata', len(statistics_data))
+# write statistics result into a file in format of LibSVM
+with open("./training_testing_data_svm_acc_vel_groupseg.txt", "w") as the_file:
+#with open("./WISDM_ar_v1.1/WISDM_ar_v1.1_raw_svm_2.txt", "a") as the_file:
+    for segdata in statistics_data:
+        print('seg_data_label',str(segdata["label"]))
+        #row = str(STATUS.index(str(seg_data["label"])[3:5])) + " " + \
+
+        row = str(STATUS.index(str(segdata["label"])[(len(str(segdata["label"]))-2):len(str(segdata["label"]))])+1) + " " + \
+              str(FEATURE.index("mean")+1) + ":" + str(segdata["mean"]) + " " + \
+              str(FEATURE.index("max")+1) + ":" + str(segdata["max"]) + " " + \
+              str(FEATURE.index("min")+1) + ":" + str(segdata["min"]) + " " + \
+              str(FEATURE.index("std")+1) + ":" + str(segdata["std"]) + " " + \
+              str(FEATURE.index("v_mean")+1) + ":" + str(segdata["v_mean"]) + " " + \
+              str(FEATURE.index("v_max")+1) + ":" + str(segdata["v_max"]) + " " + \
+              str(FEATURE.index("v_min")+1) + ":" + str(segdata["v_min"]) + " " + \
+              str(FEATURE.index("v_std")+1) + ":" + str(segdata["v_std"]) + "\n"
+        #print(row)
+        the_file.write(row)
